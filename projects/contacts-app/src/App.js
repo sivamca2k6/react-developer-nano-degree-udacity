829509868b5d7ed9b6b046ab5b2c3629735d3792
SIVAKUMAR JAGANATHAN <sivamca2k6.work@gmail.com>
2021-05-15 21:31:10 +0800

    ContactList component added with props

--- projects/contacts-app/src/App.js
+++ projects/contacts-app/src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//create component for display list
+class ContactList extends React.Component {
+  render() {
+    const people = this.props.contacts
+
+    return (
+      <ol>
+        {
+          people.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ol>)
+  }
 }
 
+class App extends Component {
+  render() {
+
+    return (
+            <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                 React App
+                </p>
+              </header>
+              
+                <ContactList contacts = { [ {name : "Siva"},{name : "bharathi"} ]} />
+                <ContactList contacts = { [ {name : "Siva"},{name : "bharathi"} ]} />
+              
+            </div>
+          )
+  }
+}
+
+//make avialable at outside world to reuse
 export default App;
+
