ce7d07c25a968d2d0d192515695f306fe5c56305
SIVAKUMAR JAGANATHAN <sivamca2k6.work@gmail.com>
2021-05-19 20:47:58 +0800

    ex3 -stateless-functional-compo

---
+++ lessons/react-state-exercises/ex3 -stateless-functional-compo/src/MovieCardsList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import MovieCard from './MovieCard';
+
+function MovieCardsList (props)
+ {
+    const { profiles, users, movies } = props;
+    const usersByMovie = {};
+
+    profiles.forEach(profile => {
+      const movieID = profile.favoriteMovieID;
+
+      if (usersByMovie[movieID]) {
+        usersByMovie[movieID].push(profile.userID);
+      } else {
+        usersByMovie[movieID] = [profile.userID];
+      }
+    });
+
+    const movieCards = Object.keys(movies).map(id => (
+      <MovieCard
+        key={id}
+        users={users}
+        usersWhoLikedMovie={usersByMovie[id]}
+        movieInfo={movies[id]}
+      />
+    ));
+
+    return <ul>{movieCards}</ul>;
+  }
+
+export default MovieCardsList;
