cea8e134665f64c9ff17201cce9785f0a1e58735
SIVAKUMAR JAGANATHAN <sivamca2k6.work@gmail.com>
2021-05-23 22:03:02 +0800

    Contacts app - form created and pass data with serialize to parent

--- projects/reactnd-contacts-app/frontend/src/components/AddContact.js
+++ projects/reactnd-contacts-app/frontend/src/components/AddContact.js
@@ -1,12 +1,39 @@
 import React ,{Component} from 'react'
+import { Link } from 'react-router-dom'
+import ImageInput from '../ImageInput'
+import serializeForm from 'form-serialize'
 
 class AddContact extends Component {
 
+    HandleFormSubmit = (event) =>{
+        event.preventDefault();
+
+        if (this.props.onCreateContact) {
+            this.props.onCreateContact(values); //call to parent APP.js
+          }
+    }
+
     render(){ return(
 
-        <div>
-            <h1> Add Contact</h1>
-        </div>
+    <div>
+        <Link
+          className='close-create-contact'
+          to='/'>
+            Close
+        </Link>
+        <form className='create-contact-form' onSubmit ={this.HandleFormSubmit}>
+          <ImageInput
+            className='create-contact-avatar-input'
+            name='avatarURL'
+            maxHeight={64}
+          />
+          <div className='create-contact-details'>
+            <input type='text' name='name' placeholder='Name' />
+            <input type='text' name='handle' placeholder='Handle' />
+            <button>Add Contact</button>
+          </div>
+        </form>
+      </div>
         
     )}
 }
