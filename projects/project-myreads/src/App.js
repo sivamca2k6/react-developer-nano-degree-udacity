51b22a7ec7209314fb891e5ae18642e6108b26f8
SIVAKUMAR JAGANATHAN <sivamca2k6.work@gmail.com>
2021-05-26 10:00:53 +0800

    Project Myreads - navigation router integrated

--- projects/project-myreads/src/App.js
+++ projects/project-myreads/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Route,Link, Switch } from 'react-router-dom'
 // import * as BooksAPI from './utils/BooksAPI'
 import './App.css'
 import BookShelfList from './components/BookShelfList'
 import BookSearch from './components/BookSearch'
+
 
 class BooksApp extends React.Component {
   state = {
@@ -11,20 +13,24 @@ class BooksApp extends React.Component {
 
   render() {
     return (
-      <div className="app">
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
+        <div className="app">
+            <div className="list-books">
+              <div className="list-books-title">
+                <h1>MyReads</h1>
+              </div>
+              
+              <Switch>
+                <Route exact path='/'> 
+                  <BookShelfList /> 
+                </Route>
+                <Route exact path='/Search' component={BookSearch} />
+              </Switch>
 
-            <BookShelfList />
-            <BookSearch />
-
-            <div className="open-search">
-              <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+              <div className="open-search">
+                <Link to='/Search'> <button></button> </Link>
+              </div>
             </div>
-          </div>
-      </div>
+        </div>
     )
   }
 }
--- projects/project-myreads/src/components/BookSearch.js
+++ projects/project-myreads/src/components/BookSearch.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react'
+import { Link } from 'react-router-dom'
 
 
 class BookSearch extends Component{
@@ -7,14 +8,14 @@ class BookSearch extends Component{
 
         <div className="search-books">
             <div className="search-books-bar">
-                <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
+                 <Link to='/' className="close-search" />
                 <div className="search-books-input-wrapper">
                     <input type="text" placeholder="Search by title or author"/>
                 </div>
                 </div>
                 <div className="search-books-results">
-                    <ol className="books-grid"></ol>
-                </div>
+                <ol className="books-grid"></ol>
+             </div>
         </div>
 
     )}
