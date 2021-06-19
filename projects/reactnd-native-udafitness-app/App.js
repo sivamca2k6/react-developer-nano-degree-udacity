943c5fd5ce401e8b7cb81672d728b03380b1cd85
SIVAKUMAR JAGANATHAN <sivamca2k6.work@gmail.com>
2021-06-19 20:57:44 +0800

    native udafitness app  - Addentry and helpers updated

--- projects/reactnd-native-udafitness-app/App.js
+++ projects/reactnd-native-udafitness-app/App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { View } from 'react-native'
+import AddEntry from './components/AddEntry'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Super</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <AddEntry />
+      </View>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
