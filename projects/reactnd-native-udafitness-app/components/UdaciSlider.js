297cf8a7adde2ef79fdb1e8257251e889e244d6c
SIVAKUMAR JAGANATHAN <sivamca2k6.work@gmail.com>
2021-06-20 14:18:04 +0800

    reactnd-native-udafitness-app - slider added

--- projects/reactnd-native-udafitness-app/components/UdaciSlider.js
+++ projects/reactnd-native-udafitness-app/components/UdaciSlider.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import Slider from '@react-native-community/slider';
 
-export default function UdaciSlider () {
+export default function UdaciSlider ({max,unit,step,value,onChange}) {
   return (
     <View>
-      <Text>UdaciSlider</Text>
+      <Slider
+        step={step}
+        value={value}
+        maximumValue={max}
+        minimumValue={0}
+        onValueChange={onChange}
+      />
+      <View>
+        <Text>{value}</Text>
+        <Text>{unit}</Text>
+      </View>
     </View>
   )
 }
