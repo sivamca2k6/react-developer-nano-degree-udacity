630e9070b558b3b9427844be62364de0b232afe7
SIVAKUMAR JAGANATHAN <sivamca2k6.work@gmail.com>
2021-06-22 20:38:59 +0800

    reactnd-native-udafitness-app - style applied for rest

--- projects/reactnd-native-udafitness-app/components/DateHeader.js
+++ projects/reactnd-native-udafitness-app/components/DateHeader.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Text } from 'react-native'
+import { purple, gray, white } from '../utils/colors'
 
 export default function DateHeader ({ date }) {
   return (
-    <Text>
+    <Text style={{color: purple, fontSize: 25 , textAlign:'center'}}>
       {date}
     </Text>
   )
